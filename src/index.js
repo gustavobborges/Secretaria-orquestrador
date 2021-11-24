b21c96447dacec1cdac1fd657ae143e46857d498
gustavo <gustavoborges98@hotmail.com>
2021-11-24 01:24:36 -0300

    rota de envio de mensagem

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+const express = require('express');
 const venom = require('venom-bot');
-const puppeteer = require('puppeteer');
+const cors = require("cors");
+// const puppeteer = require('puppeteer');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(8080);
+
+router.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+	app.use(cors());
+	next();
+});
+
 
 venom
   .create({
@@ -12,7 +31,8 @@ venom
     console.log(erro);
   });
 
-async function start(client) {
+function start(client) {
+
   client.onMessage((message) => {
     if (message.body === 'Hi' && message.isGroupMsg === false) {
       client
@@ -25,4 +45,21 @@ async function start(client) {
         });
     }
   });
+
+  app.route("/sendMessage")
+		.post(function (req, res) {
+			try {
+				const phone = '55' + req.body.phone + '@c.us';
+				const messageText = req.body.messageText;
+				client.sendText(phone, messageText);
+				// const phoneInput = messageSended.me.wid.user;
+        console.log('telefone')
+				res.json({
+          messageSended: 'enviado!',
+          // origin: phoneInput
+        });
+			} catch (err) {
+				return res.status(400).json({ error: 'Houve algum problema durante a requisição' })
+			}
+		});
 }
